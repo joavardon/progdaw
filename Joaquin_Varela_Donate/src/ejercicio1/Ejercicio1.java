dfa4efdf3d1f9d4de10e6db711027ec8c540210c
Admin <Admin@L300>
2014-06-23 03:45:18 +0200

    23-6-14

--- Joaquin_Varela_Donate/src/ejercicio1/Ejercicio1.java
+++ Joaquin_Varela_Donate/src/ejercicio1/Ejercicio1.java
@@ -7,7 +7,8 @@ public class Ejercicio1 {
 	
 	public static void main(String[] args) {
 		Scanner teclado=new Scanner(System.in);
-		String cadena="En un lugar, donde no se podía soñar...";
+		System.out.println("Introduce una cadena: ");
+		String cadena=teclado.nextLine();
 		System.out.println("Resultado: ");
 		System.out.println("\t"+miniMix(cadena));
 
@@ -16,26 +17,23 @@ public class Ejercicio1 {
 	private static String miniMix(String cadena) {
 		StringBuilder micadena=new StringBuilder ();
 		int c=1;
+		char caracter;
 		for(int i=0;i<cadena.length();i++){
-			String cadena1="";
-			String cadena2="";
-			String letra="";
 			if(Character.isLetter(cadena.charAt(i))){
-				cadena1=""+cadena.charAt(i);
-					if(c%2!=0 || c==1){
-						letra=cadena1.toUpperCase();
-						micadena.append(letra);
+					if(c%2!=0){
+						caracter=Character.toUpperCase(cadena.charAt(i));
+						micadena.append(caracter);
 						c++;
 					}
 					else{
-						letra=cadena1.toLowerCase();
-						micadena.append(letra);
+						caracter=Character.toLowerCase(cadena.charAt(i));
+						micadena.append(caracter);
 						c++;
 					}
 				}
 			else{
-				cadena2=""+cadena.charAt(i);
-				micadena.append(cadena2);
+				caracter=cadena.charAt(i);
+				micadena.append(caracter);
 			}
 		}		
 		return micadena.toString();
